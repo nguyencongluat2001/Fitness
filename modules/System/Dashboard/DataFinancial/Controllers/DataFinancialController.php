8240770cda08fb74cc72bea389dc5feee2ef14a4
luatnc <luatncth2007024@fpt.edu.vn>
2023-08-06 06:48:05 -0700

    f

--- modules/System/Dashboard/DataFinancial/Controllers/DataFinancialController.php
+++ modules/System/Dashboard/DataFinancial/Controllers/DataFinancialController.php
@@ -24,6 +24,7 @@ class DataFinancialController extends Controller
     ){
         $this->DataFinancialService = $DataFinancialService;
         $this->categoryService = $categoryService;
+        
     }
 
     /**
