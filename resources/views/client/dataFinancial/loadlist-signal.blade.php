5e587a83755d656324ea1a3a4b3b10a794412bf9
nguyencongluat092001@gmail.com <luatnc@efy.com.vn>
2023-08-31 09:59:02 +0700

    f

--- resources/views/client/dataFinancial/loadlist-signal.blade.php
+++ resources/views/client/dataFinancial/loadlist-signal.blade.php
@@ -60,7 +60,7 @@
   #style-1 #table-data thead tr td {
     position: sticky;
     top: 0;
-    background: #6add58;
+    background: #529845;
   }
 </style>
 <div id="style-1" style="padding-right:10px;">
@@ -87,7 +87,7 @@
             <center>FINTOP - DANH MỤC CỔ PHIẾU CHO TÍN HIỆU</center>
           </th>
         </tr>
-        <tr style="background:#6add58;color:white">
+        <tr style="background:#529845;color:white">
           <td style="white-space: inherit;vertical-align: middle" align="center"><b>STT</b></td>
           <!-- <td style="white-space: inherit;vertical-align: middle;background: #00ad34;animation: lights 2s 750ms linear infinite;" align="center"><b> </i>Nhập mã cổ phiếu</b> <br> <i class="fas fa-angle-double-down"></td> -->
           <td style="white-space: inherit;vertical-align: middle" align="center"><b>Mã CP</b></td>
