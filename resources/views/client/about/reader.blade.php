49ccaaf5aea3451c3b36876ca38de1ea26220c84
luatnc <luatncth2007024@fpt.edu.vn>
2023-09-26 09:09:55 -0700

    f

--- resources/views/client/about/reader.blade.php
+++ resources/views/client/about/reader.blade.php
@@ -37,59 +37,57 @@ use Carbon\Carbon;
     <!-- Start Banner Hero -->
     @php Carbon::setLocale('vi');$now = Carbon::now(); $created_at = Carbon::create($datas['blogDetail']->created_at) @endphp
 
-    <section class="container" style="background:#ffffff8a">
-        <div class="container banner-wrapper" style="background: white;">
-            <div id="index_banner_detail" class="banner-vertical-center-index">
-                <!-- Start slider -->
-                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner active" >
-                        <!-- End Contact -->
-                        <div class="carousel-item active list-hispital-home" >
-                            <div class=" row d-flex align-items-center">
-                                <div class="banner-content col-lg-12 col-md-8 m-lg-auto text-left ">
-                                    <div class="row g-lg-5 mb-4">
-                                        <div class="banner-wrapper w-100" style="background: #ffffff;">
-                                            <div class="card-header pb-0 p-0 pt-3">
-                                                <div class="">
-                                                    <ul class="list-group">
-                                                        <div  class="col-sm-6 col-lg-12 text-decoration-none">
-                                                            <div class="pb-3 d-lg-flex gx-5 col-md-10" style="margin: auto;">
-                                                                <div class="col-lg-3 ">
-                                                                @if((isset($datas['type_blog']) && $datas['type_blog'] == 'VIP'))
-                                                                    <h1 style="position: absolute;">
-                                                                        <img  src="{{url('/clients/img/vip.png')}}" alt="Image" style="height: 50px;width: 32px;object-fit: cover;">
-                                                                    </h1>
-                                                                    @endif
-                                                                    <img class="card-img-top" src="{{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }}" style="height: 200px;object-fit: cover;padding-right:10px" alt="...">
-                                                                </div>
-                                                                <div class="col-lg-9">
-                                                                    <h5 style="padding-top:10px;color:#000951;font-size: 30px;font-family: serif;font-weight: 600;">{{ $datas['blogDetail']->title }}</h5>
-                                                                    <p style="color: #006849;">Đăng lúc: {{$created_at->diffForHumans($now)}}</p>
-                                                                </div>
+    <div class="container banner-wrapper" style="background: white;">
+        <div id="index_banner_detail" class="banner-vertical-center-index">
+            <!-- Start slider -->
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner active" >
+                    <!-- End Contact -->
+                    <div class="carousel-item active list-hispital-home" >
+                        <div class=" row d-flex align-items-center">
+                            <div class="banner-content col-lg-12 col-md-8 m-lg-auto text-left ">
+                                <div class="row g-lg-5 mb-4">
+                                    <div class="banner-wrapper w-100" style="background: #ffffff;">
+                                        <div class="card-header pb-0 p-0 pt-3">
+                                            <div class="">
+                                                <ul class="list-group">
+                                                    <div  class="col-sm-6 col-lg-12 text-decoration-none">
+                                                        <div class="pb-3 d-lg-flex gx-5 col-md-10" style="margin: auto;">
+                                                            <div class="col-lg-3 ">
+                                                            @if((isset($datas['type_blog']) && $datas['type_blog'] == 'VIP'))
+                                                                <h1 style="position: absolute;">
+                                                                    <img  src="{{url('/clients/img/vip.png')}}" alt="Image" style="height: 50px;width: 32px;object-fit: cover;">
+                                                                </h1>
+                                                                @endif
+                                                                <img class="card-img-top" src="{{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }}" style="height: 200px;object-fit: cover;padding-right:10px" alt="...">
+                                                            </div>
+                                                            <div class="col-lg-9">
+                                                                <h5 style="padding-top:10px;color:#000951;font-size: 30px;font-family: serif;font-weight: 600;">{{ $datas['blogDetail']->title }}</h5>
+                                                                <p style="color: #006849;">Đăng lúc: {{$created_at->diffForHumans($now)}}</p>
                                                             </div>
                                                         </div>
-                                                    </ul>
-                                                </div>
+                                                    </div>
+                                                </ul>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
-                        <!-- Start Banner Hero -->
                     </div>
-                </div>
-            </div>
-            <div class="banner-vertical-center-work container d-flex justify-content-center align-items-center" >
-            <div class="banner-content col-lg-10 col-10 m-lg-auto text-left content-reader">
-                <div style="color:#264451; width: 100%;" class="light-300 text-justify">
-                {!! $datas['blogDetail']->decision !!}
+                    
+                    <!-- Start Banner Hero -->
                 </div>
             </div>
         </div>
+        <div class="banner-vertical-center-work container d-flex justify-content-center align-items-center" >
+        <div class="banner-content col-lg-10 col-10 m-lg-auto text-left content-reader">
+            <div style="color:#264451; width: 100%;" class="light-300 text-justify">
+               {!! $datas['blogDetail']->decision !!}
+            </div>
         </div>
-    </section>
+    </div>
+    </div>
     <!-- End Service -->
 </div>
 <script>
