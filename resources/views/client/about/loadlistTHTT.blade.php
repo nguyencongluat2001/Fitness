202e64208e5ff053147ab263433f5e80ddf1159c
khuong1542 <khuong1542@gmail.com>
2023-09-26 23:23:09 +0700

    update

--- resources/views/client/about/loadlistTHTT.blade.php
+++ resources/views/client/about/loadlistTHTT.blade.php
@@ -19,32 +19,38 @@ use Carbon\Carbon;
         <div id="style-1" class="scrollbar" style="padding-right:10px;max-height:900px !important">
             <ul class="list-group">
                 @foreach ($datas as $key => $data)
-                @php Carbon::setLocale('vi');$now = Carbon::now(); $created_at = Carbon::create($data->created_at) @endphp
-                <div onclick="JS_About.blogReader('{{$data->id}}')" class="col-sm-6 col-lg-12 text-decoration-none {{ $data->code_category }}" style="cursor:pointer">
+                @php
+                Carbon::setLocale('vi');$now = Carbon::now();
+                $created_at = Carbon::create($data->created_at);
+                @endphp
+                <div class="col-sm-6 col-lg-12 text-decoration-none {{ $data->code_category }}">
                     <div class="pb-3 d-lg-flex gx-5">
-                    <!-- display: flex;align-items: center;justify-content: center; -->
+                        <!-- display: flex;align-items: center;justify-content: center; -->
                         <div class="col-lg-3 " style="align-items: right;justify-content: right;position: relative;">
-                        @if((isset($data['type_blog']) && $data['type_blog'] == 'VIP'))
-                        <h1 style="position: absolute;right:0">
-                           
-                            <img  src="{{url('/clients/img/vip.png')}}" alt="Image" style="height: 60px;width: 50px;object-fit: cover;">
-                        </h1>
-                        @endif
-                            <img class="card-img-top" src="{{url('/file-image-client/blogs/')}}/{{ !empty($data->imageBlog[0]->name_image)?$data->imageBlog[0]->name_image:'' }}" style="height: 170px;width: 100%;object-fit: cover;" alt="...">
+                            <a href="{{url('/client/about/reader/') . '/' . $data->id}}">
+                                @if((isset($data['type_blog']) && $data['type_blog'] == 'VIP'))
+                                <h1 style="position: absolute;right:0">
+                                    <img src="{{url('/clients/img/vip.png')}}" alt="Image" style="height: 60px;width: 50px;object-fit: cover;">
+                                </h1>
+                                @endif
+                                <img class="card-img-top" src="{{url('/file-image-client/blogs/')}}/{{ !empty($data->imageBlog[0]->name_image)?$data->imageBlog[0]->name_image:'' }}" style="height: 170px;width: 100%;object-fit: cover;" alt="...">
+                            </a>
                         </div>
-                        <div style="width:20px">
-                        </div>
+                        <div style="width:20px"></div>
                         <div class="col-lg-7">
                             <!-- <div class="card-body"> -->
+                            <a href="{{url('/client/about/reader/') . '/' . $data->id}}">
                                 <h5 class="card-title light-600 text-dark">{{ $data->detailBlog->title }}</h5>
-                                <i>{{$created_at->diffForHumans($now)}}</i>
-                                <p class="light-300">
-                                <div class="blogReader">{!! $data->detailBlog->decision !!}</div>
-                                </p>
-                                <span class="text-decoration-none light-300" onclick="JS_About.reader('$data->id')">
+                            </a>
+                            <i>{{$created_at->diffForHumans($now)}}</i>
+                            <p class="light-300">
+                            <div class="blogReader">{!! $data->detailBlog->decision !!}</div>
+                            </p>
+                            <a href="{{url('/client/about/reader/') . '/' . $data->id}}">
+                                <span class="text-decoration-none light-300">
                                     Đọc thêm <i class='bx bxs-hand-right ms-1'></i>
                                 </span>
-                            <!-- </div> -->
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -58,5 +64,4 @@ use Carbon\Carbon;
             @endif
         </div>
     </div>
-</div>
 </div>
