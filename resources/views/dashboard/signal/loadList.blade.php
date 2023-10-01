d7dbb3fb15a57d9217f9d505561e8850a18c721a
luatnc <luatncth2007024@fpt.edu.vn>
2023-09-30 20:24:20 -0700

    f

--- resources/views/dashboard/signal/loadList.blade.php
+++ resources/views/dashboard/signal/loadList.blade.php
@@ -19,13 +19,13 @@
                         onclick="checkbox_all_item_id(document.forms[0].chk_item_id);"></td>
                 <td align="center"><b>STT</b></td>
                 <td align="center"><b>Người thêm</b></td>
-                <td align="center"><b>Tiêu đề</b></td>
+                <!-- <td align="center"><b>Tiêu đề</b></td> -->
                 <td align="center"><b>Mã cổ phiếu</b></td>
                 <td align="center"><b>Loại</b></td>
                 <td align="center"><b>Mục tiêu</b></td>
-                <td align="center"><b>Dừng lỗ</b></td>
                 <td align="center"><b>Giá mua</b></td>
-                <td align="center"><b>Sắp xếp</b></td>
+                <td align="center"><b>Dừng lỗ</b></td>
+                <!-- <td align="center"><b>Sắp xếp</b></td> -->
                 <td align="center"><b>Trạng thái</b></td>
                 <td align="center"><b><span onclick="JS_Signal.addrow()" class="text-cursor text-primary"><i class="fas fa-plus-square"></i></span></b></td>
             </tr>
@@ -39,9 +39,9 @@
                                 value="{{ $data->id }}"></td>
                         <td align="center">{{($datas->currentPage() - 1)*$datas->perPage() + ($key + 1)}}</td>
                         <td onclick="{select_row(this);}">{{ isset($data->users->name) ? $data->users->name : '' }}</td>
-                        <td class="td_title_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'title')">
+                        <!-- <td class="td_title_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'title')">
                             <span id="span_title_{{$id}}" class="span_title_{{$id}}">{{ $data->title }}</span>
-                        </td>
+                        </td> -->
                         <td class="td_code_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'code')">
                             <span id="span_code_{{$id}}" class="span_code_{{$id}}">{{ $data->code }}</span>
                         </td>
@@ -51,15 +51,15 @@
                         <td class="td_target_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'target')">
                             <span id="span_target_{{$id}}" class="span_target_{{$id}}">{{ $data->target }}</span>
                         </td>
-                        <td class="td_stop_loss_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'stop_loss')">
-                            <span id="span_stop_loss_{{$id}}" class="span_stop_loss_{{$id}}">{{ $data->stop_loss }}</span>
-                        </td>
                         <td class="td_price_buy_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'price_buy')">
                             <span id="span_price_buy_{{$id}}" class="span_price_buy_{{$id}}">{{ $data->price_buy }}</span>
                         </td>
-                        <td class="text-center td_order_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'order')">
-                            <span id="span_order_{{$id}}" class="span_order_{{$id}}">{{ $data->order }}</span>
+                        <td class="td_stop_loss_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'stop_loss')">
+                            <span id="span_stop_loss_{{$id}}" class="span_stop_loss_{{$id}}">{{ $data->stop_loss }}</span>
                         </td>
+                        <!-- <td class="text-center td_order_{{$id}}" onclick="{select_row(this);}" ondblclick="click2('{{$id}}', 'order')">
+                            <span id="span_order_{{$id}}" class="span_order_{{$id}}">{{ $data->order }}</span>
+                        </td> -->
                         <td onclick="{select_row(this);}" align="center">
                             <label class="custom-control custom-checkbox p-0 m-0 pointer " style="cursor: pointer;">
                                 <input type="checkbox" hidden class="custom-control-input toggle-status" id="status_{{$id}}" data-id="{{$id}}" {{ $data->status == 1 ? 'checked' : '' }}>
