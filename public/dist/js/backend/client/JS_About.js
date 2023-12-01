af949bd5f6afbce6a507cf9ef6bbbc62ed7d7bc1
nguyencongluat092001@gmail.com <luatnc@efy.com.vn>
2023-12-01 16:39:39 +0700

    f

--- public/dist/js/backend/client/JS_About.js
+++ public/dist/js/backend/client/JS_About.js
@@ -81,7 +81,7 @@ JS_About.prototype.loadListTHTT = function (oFormBlog,numberPage = 1, perPage = 
  */
 JS_About.prototype.loadListTKP = function (oFormBlog,numberPage = 1, perPage = 15) {
     var myClass = this;
-    var url = myClass.urlPath + '/loadListPTCP';
+    var url = myClass.urlPath + '/loadListTKP';
     var data = $(oFormBlog).serialize();
     data += '&offset=' + numberPage;
     data += '&limit=' + perPage;
