2bd812f43dcb30e5cdd31f9ef839402b95c56763
luatnc <luatncth2007024@fpt.edu.vn>
2023-12-04 07:49:24 -0800

    f

--- public/dist/js/backend/client/JS_About.js
+++ public/dist/js/backend/client/JS_About.js
@@ -81,11 +81,11 @@ JS_About.prototype.loadListTHTT = function (oFormBlog,numberPage = 1, perPage = 
  */
 JS_About.prototype.loadListTKP = function (oFormBlog,numberPage = 1, perPage = 15) {
     var myClass = this;
-    var url = myClass.urlPath + '/loadListTKP';
+    var url = myClass.urlPath + '/BAO_CAO_PTDTVIP';
     var data = $(oFormBlog).serialize();
     data += '&offset=' + numberPage;
     data += '&limit=' + perPage;
-    data += '&category=BAO_CAO_PTDTVIP';
+    data += '&category=BAO_CAO_TKP';
     $.ajax({
         url: url,
         type: "GET",
@@ -139,7 +139,7 @@ JS_About.prototype.loadListPTN = function (oFormBlog,numberPage = 1, perPage = 1
     });
 }
 /**
- * Danh sách Phân tích cổ phiếu
+ * Danh sách Phân tích doanh nghiep
  *
  * @param oForm (tên form)
  *
@@ -147,11 +147,11 @@ JS_About.prototype.loadListPTN = function (oFormBlog,numberPage = 1, perPage = 1
  */
 JS_About.prototype.loadListPTCP = function (oFormBlog,numberPage = 1, perPage = 15) {
     var myClass = this;
-    var url = myClass.urlPath + '/loadListPTCP';
+    var url = myClass.urlPath + '/BAO_CAO_PTDN';
     var data = $(oFormBlog).serialize();
     data += '&offset=' + numberPage;
     data += '&limit=' + perPage;
-    data += '&category=BAO_CAO_PTCP';
+    data += '&category=BAO_CAO_PTDN';
     $.ajax({
         url: url,
         type: "GET",
