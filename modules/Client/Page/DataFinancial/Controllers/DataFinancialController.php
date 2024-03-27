d355af500ca3467df28d6c173426dc899a51775f
luatnc <luatnc@efy.com.vn>
2024-03-27 10:30:20 +0700

    f

--- modules/Client/Page/DataFinancial/Controllers/DataFinancialController.php
+++ modules/Client/Page/DataFinancial/Controllers/DataFinancialController.php
@@ -319,7 +319,7 @@ class DataFinancialController extends Controller
     {
         $arrInput = $request->input();
         $data = [];
-        $result['datas'] = $this->effectiveService->where('status','!=','')->orderBy('created_at','DESC')->get();
+        $result['datas'] = $this->effectiveService->where('status','!=','')->orderBy('date_close','DESC')->get();
         foreach($result['datas'] as $item){
             $getCate = $this->categoryService->where('code_category',$item['code_category'])->first();
             $data['datas'][] = [
