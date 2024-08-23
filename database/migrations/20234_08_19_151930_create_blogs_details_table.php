042fef957e8b3661993df3920d722835cd603980
Luật Nguyễn <luatnc@efy.com.vn>
2024-08-23 14:27:22 +0700

    f

--- database/migrations/20234_08_19_151930_create_blogs_details_table.php
+++ database/migrations/20234_08_19_151930_create_blogs_details_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs_details', function (Blueprint $table) {
+        Schema::table('blogs_details', function (Blueprint $table) {
             $table->longText('decision')->change();
         });
     }
