df97d30d3ada5f805e2e0863e745c091237bc819
Luật Nguyễn <luatnc@efy.com.vn>
2024-12-05 17:02:20 +0700

    f

--- resources/views/client/home/home.blade.php
+++ resources/views/client/home/home.blade.php
@@ -252,8 +252,8 @@ use Carbon\Carbon;
                             </a>
                         </div>
                         <div class="about-content">
-                            <div><i>{{ $data->users->name ?? '' }} | {{$created_at->diffForHumans($now)}} </i></div>
-                            <span style="font-size: 10px;color: #9f9292;"><i class="far fa-eye"></i> {{ $data->view_click }}</span>
+                            <div><i>{{ $data->users->name ?? '' }} | {{$created_at->diffForHumans($now)}} <span style="font-size: 10px;color: #9f9292;"><i class="far fa-eye"></i> {{ $data->view_click }}</span></i></div>
+                            
                             <a href="{{url('/client/about/reader/') . '/' . $data->id}}">
                                 <h5 class="card-title light-600 text-dark">{{ $data->detailBlog->title }}</h5>
                             </a>
