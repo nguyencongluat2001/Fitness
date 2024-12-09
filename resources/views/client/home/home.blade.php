f84927f918881c1872f7d0769f6e2bd406705b52
luatnc <luatncth2007024@fpt.edu.vn>
2024-12-10 00:28:31 +0700

    f

--- resources/views/client/home/home.blade.php
+++ resources/views/client/home/home.blade.php
@@ -242,6 +242,7 @@ use Carbon\Carbon;
                         <button  type="button" class="btn btn-success" href="{{ url('/client/privileges/index') }}"> <a href="{{ url('/client/privileges/index') }}" style="animation: lights 2s 750ms linear infinite;">Đăng ký</a></button>
                     </span>
                 @endif
+                <br>
                 @if((isset($_SESSION['id']) && $_SESSION['account_type_vip'] == 'VIP2'))
                        @if(isset($VIP))
                         @foreach ($VIP as $key => $data)
