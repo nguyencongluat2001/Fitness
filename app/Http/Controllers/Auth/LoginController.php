cd12deededf6fa3c916ecc585624723ac86f8b09
Luật Nguyễn <luatnc@efy.com.vn>
2024-12-25 15:29:04 +0700

    fix client/home/index

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -140,7 +140,7 @@ class LoginController extends Controller
                 $_SESSION["sidebar"] = $sideBar;
                 Auth::login($user);
                 // return redirect('system/home/index');
-                return redirect('client/home/index');
+                return redirect('/');
             }elseif($user->role == 'USERS' || $user->role == 'USER'){
                 $_SESSION["role"] = $user->role;
                 $_SESSION["id_personnel"] = $getUsers->id_personnel;
@@ -151,7 +151,7 @@ class LoginController extends Controller
                 $_SESSION["color_view"] = !empty($getInfo->color_view)?$getInfo->color_view:2;
                 $checkPrLogin = $this->permission_login($email);
                 Auth::login($user);
-                return redirect('client/home/index');
+                return redirect('/');
             }
         }
         if (Auth::attempt(['email' => $email, 'password' => $password])) {
@@ -180,7 +180,7 @@ class LoginController extends Controller
                 $sideBar = $this->checkPermision($sideBarConfig , $user);
                 $_SESSION["sidebar"] = $sideBar;
                 Auth::login($user);
-                return redirect('client/home/index');
+                return redirect('/');
             }elseif($user->role == 'USERS' || $user->role == 'USER'){
                 $_SESSION["role"] = $user->role;
                 $_SESSION["id_personnel"] = $getUsers->id_personnel;
@@ -191,7 +191,7 @@ class LoginController extends Controller
                 $_SESSION["color_view"] = !empty($getInfo->color_view)?$getInfo->color_view:2;
                 $checkPrLogin = $this->permission_login($email);
                 Auth::login($user);
-                return redirect('client/home/index');
+                return redirect('/');
             }
         } else {
             $data['message'] = "Sai tên đăng nhập hoặc mật khẩu!";
