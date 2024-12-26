f705130ad4fb3a4ffde9566b3840ce03d1b3f298
Luật Nguyễn <luatnc@efy.com.vn>
2024-12-26 15:31:15 +0700

    f

--- resources/views/client/about/reader.blade.php
+++ resources/views/client/about/reader.blade.php
@@ -6,9 +6,14 @@ use Carbon\Carbon;
 <!-- <title>{{ $datas['blogDetail']->title }}</title> -->
 <meta property="og:title" content="{{ $datas['blogDetail']->title }}">
 <meta property="og:description" content="{{ $datas['blogDetail']->title }}">
-<meta property="og:image" content="{{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }}" />
+<!-- <meta property="og:image" content="{{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }}" /> -->
 <meta property="og:image:width" content="640">
 <meta property="og:image:height" content="400">
+@section('og:image', {{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }})
+@section('og:url', url()->current())
+
+
+
 <style>
     #content-reader iframe  {
         width: 100% !important;
--- resources/views/client/layouts/index.blade.php
+++ resources/views/client/layouts/index.blade.php
@@ -10,13 +10,17 @@
     <meta property="og:type" content="website">
     <!-- <meta property="og:url" content="https://fintopdata.vn"> -->
     <meta property="fb:app_id" content="2115022521881483">
+
+    <meta property="og:image" content="@yield('og:image', 'https://fintopdata.vn/clients/img/LogoFinTop_notbg.jpg'))" />
+    <meta property="og:image:secure_url" content="@yield('og:image', 'https://fintopdata.vn/clients/img/LogoFinTop_notbg.jpg'))" />
+
     <!-- <meta property="og:title" content="Trung tâm NC&PT Dữ liệu chứng khoán FinTop">
     <meta property="og:description" content="Dữ liệu chứng khoán FinTop"> -->
     <?php $current_url = Request::url(); ?>
     @if($current_url == 'https://fintopdata.vn' || $current_url == 'https://fintopdata.vn/' || $current_url == 'https://fintopdata.vn/client/home/index')
         <!-- <meta property="og:image" content="https://fintopdata.vn/clients/img/LogoFinTop_notbg.jpg" /> -->
-        <meta property="og:image:width" content="640">
-        <meta property="og:image:height" content="400">
+        <!-- <meta property="og:image:width" content="640">
+        <meta property="og:image:height" content="400"> -->
     @endif
 
 
