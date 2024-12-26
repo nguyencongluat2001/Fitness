22e47eb8280123471dd1e6f8a9dd6f81bdb83c41
Luật Nguyễn <luatnc@efy.com.vn>
2024-12-26 15:38:30 +0700

    f

--- resources/views/client/about/reader.blade.php
+++ resources/views/client/about/reader.blade.php
@@ -1,5 +1,7 @@
 @php
 use Carbon\Carbon;
+@section('og:image', {{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }})
+@section('og:url', url()->current())
 @endphp
 @extends('client.layouts.index')
 @section('body-client')
@@ -9,8 +11,7 @@ use Carbon\Carbon;
 <!-- <meta property="og:image" content="{{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }}" /> -->
 <meta property="og:image:width" content="640">
 <meta property="og:image:height" content="400">
-@section('og:image', "{{url('/file-image-client/blogs/')}}/{{ !empty($datas['blogImage']->name_image)?$datas['blogImage']->name_image:'' }}")
-@section('og:url', url()->current())
+
 
 
 
